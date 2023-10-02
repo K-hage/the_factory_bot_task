e48183fa6be77babcb064ea7638e4f86c1a0874a
KHAGE <khazhanyants@mail.com>
2023-10-02 06:26:33 +0300

    add name for urls

--- tg_bot/urls.py
+++ tg_bot/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from tg_bot import views
 
 urlpatterns = [
-    path('tg_token/', views.GetGenerateToken.as_view()),
-    path('send_message/', views.SendMessage.as_view()),
-    path('list_messages/', views.MessagesListAPI.as_view()),
+    path('tg_token/', views.GetGenerateToken.as_view(), name='generate token'),
+    path('send_message/', views.SendMessage.as_view(), name='send message'),
+    path('list_messages/', views.MessagesListAPI.as_view(), name='list message'),
 ]
