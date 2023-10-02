b812abb58c0519f24357caea95d34465a0df70ec
KHAGE <khazhanyants@mail.com>
2023-10-02 08:46:17 +0300

    fix

--- users/views/logout.py
+++ users/views/logout.py
@@ -1,4 +1,4 @@
-from django.contrib.auth import logout
+from django.contrib.auth import logout, get_user_model
 from rest_framework import (
     permissions,
     status
@@ -6,8 +6,12 @@ from rest_framework import (
 from rest_framework.generics import DestroyAPIView
 from rest_framework.response import Response
 
+from users.serializers import LoginSerializer
+
 
 class LogoutView(DestroyAPIView):
+    queryset = get_user_model().objects.all
+    serializer_class = LoginSerializer
     permission_classes = [permissions.IsAuthenticated]
 
     def delete(self, request, *args, **kwargs):
